fa25fa6f7a8f526bf67ea53cee80d7ffdfc13acd
Ayoub Achak <ayoub.achak01@gmail.com>
2025-11-09 23:23:21 +0100

    chore: update Jest configuration to exclude additional paths and files from testing; increment package versions for core, Angular, and React frameworks, and add uuid dependency

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)',
+    '**/?(*.)+(spec|test).ts?(x)',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/examples/',
+    'packages/frameworks/flutter',
+    '\\.d\\.ts$', // Exclude .d.ts files
+  ],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.json',
@@ -12,9 +22,15 @@ module.exports = {
     '!packages/**/dist/**',
     '!packages/frameworks/flutter/**',
     '!**/*.d.ts',
+    '!examples/**',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@agentbridge/(.*)$': '<rootDir>/packages/$1/src',
   },
+  // Exclude examples and dist from module resolution to avoid naming collisions
+  modulePathIgnorePatterns: [
+    '<rootDir>/examples/',
+    '<rootDir>/packages/.*/dist/',
+  ],
 }; 
